2b687035efdd3baee2f4aab448b4c38117235854
viswanath <links96.4u@outlook.com>
2023-03-09 16:55:42 +0530

    Add project files.

---
+++ AlarmConsole/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlarmConsole;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+string seturl = (Environment.GetCommandLineArgs().Count() > 1)? Environment.GetCommandLineArgs()[1]:"";
+
+if (seturl != "seturl")
+{
+    string url = new UrlService().GetURl();
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        url = url.Replace("&", "^&");
+        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        Console.WriteLine($"Alarm Console started at url {url}");
+
+    }
+    return;
+}
+
+string c = "";
+
+do
+{
+    int count = 0;
+    foreach (var item in new UrlService().GetAllURLs())
+    {
+        Console.WriteLine($"Id: {++count} name: {item.Key} url: {item.Value}");
+    }
+
+    Console.WriteLine("");
+    Console.WriteLine("Press 1 to Add url.");
+    Console.WriteLine("Press 2 to Remove url.");
+    Console.WriteLine("Press 3 Open url.");
+    int opt;
+    int.TryParse(Console.ReadLine(),out opt);
+
+    if (opt == 1)
+    {
+        string name, url;
+        Console.WriteLine("Enter Name");
+        name = Console.ReadLine();
+        Console.WriteLine("Enter URL");
+        url = Console.ReadLine();
+
+        await new UrlService().AddNewURL(name, url);
+
+    }
+    else if (opt ==2)
+    {
+        int id=0,delId=0;
+        Console.WriteLine("Enter Id to delete");
+        int.TryParse(Console.ReadLine(),out id);
+
+        foreach (var item in new UrlService().GetAllURLs())
+        {
+            if (++delId== id)
+            {
+                await new UrlService().DelOneURL(item.Key,item.Value);
+            }
+        }
+    }
+    else if (opt ==3)
+    {
+        int id = 0;
+        Console.WriteLine("Enter Id to open.");
+        int.TryParse(Console.ReadLine(), out id);
+
+        string url = new UrlService().GetURl(id);
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            url = url.Replace("&", "^&");
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            Console.WriteLine($"Alarm Console started at url {url}");
+
+        }
+    }
+
+    Console.WriteLine("Start over?");
+    c = Console.ReadLine();
+    Console.Clear();
+} while (c == "y");
---
+++ AlarmConsole/UrlService.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AlarmConsole
+{
+    class UrlService
+    {
+        private Dictionary<string, string> videos
+            = JsonSerializer.Deserialize<Dictionary<string, string>>(System.IO.File.ReadAllText("urls.json"));
+
+
+        public string GetURl()
+        {
+            return videos.ElementAt(Random.Shared.Next(videos.Count)).Value;
+        }
+        public string GetURl(int id)
+        {
+            return videos.ElementAt(id-1).Value;
+        }
+        public Dictionary<string, string> GetAllURLs()
+        {
+            return videos;
+        }
+        public async Task<Dictionary<string, string>> AddNewURL(string name, string url)
+        {
+            videos.Add(name, url);
+            await UpdateUrlJsonFile();
+
+            return videos;
+        }
+
+        public async Task<Dictionary<string, string>> DelOneURL(string name, string url)
+        {
+            videos.Remove(name);
+            await UpdateUrlJsonFile();
+            return videos;
+        }
+
+        private async Task UpdateUrlJsonFile()
+        {
+            if (File.Exists(@"urls.json"))
+            {
+                File.Delete(@"urls.json");
+            }
+            await File.WriteAllLinesAsync("urls.json", new List<string> { JsonSerializer.Serialize(videos) });
+        }
+
+    }
+}
